e57e079da363cf71e2264ac44f7440a9ba250bdf
Beth Dickerson <hairdresser803@gmail.com>
2016-08-07 17:27:25 -0400

    added php functionality

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+$days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+$dayStart = "2016-08-07";
+$result = [
+	"2016-08-07" => [
+		"go to brunch", 
+		"meet up with jamie and tori"
+		],
+	"2016-08-09" => [
+		"back to work",
+		"go rock climbing"
+		],
+	"2016-08-10" => [
+		"rock climbing gym closes :("
+		]
+];
+function toItalics($word){
+	$word = "<em>" . $word . "</em>";
+	return $word;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,30 +29,22 @@
 	<body>
 		<div class="calendar-week">
 			<div class="calendar-week-title">
-				<h1 class="tues">This Week</h1>
+				<h1>This <?php echo toItalics("Week");?></h1>
 			</div>
 			<ul class="calendar-week-list">
-				<li class= "calendar-week-list-day">
-					<h4>Sunday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Monday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Tuesday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Wednesday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Thursday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Friday</h4>
-				</li>
-				<li class= "calendar-week-list-day">
-					<h4>Saturday</h4>
-				</li>
+				<?php foreach($days as $dayIndex => $day): ?>
+					<li class="calendar-week-list-day">
+						<h4><?php echo toItalics($day);?></h4>
+						<?php if(isset($result[$dayIndex])): ?>
+							<ol>
+								<?php foreach($result[$dayIndex] as $description): ?>
+									<li> <?php echo $description; ?>
+									</li>
+								<?php endforeach; ?>
+							</ol>
+						<?php endif; ?>
+					</li>
+				<?php endforeach; ?>
 			</ul>
 		</div>
 
